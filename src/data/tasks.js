b0c25db79aff541595fef8ec44c6dc89167cfcb3
Fernando Mendoza <gokuqa@gmail.com>
2022-10-03 14:21:57 -0500

    first commint

---
+++ src/data/tasks.js
@@ -0,0 +1,18 @@
+export const tasks=[
+{
+    id:0,
+    title:'mi primer tarea',
+    description: "Mi Primera tarea"
+},
+{
+    id:1,
+    title:'mi Segunda tarea',
+    description: "Mi Segunda tarea"
+},
+{
+    id:2,
+    title:'mi tercera tarea',
+    description: "Mi tercera tarea"
+}
+];
+
